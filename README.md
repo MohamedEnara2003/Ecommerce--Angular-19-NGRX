13b233ac7b685e20f812131891cab25ad4b297c6
mohamed enara <mohamedenara2003@gmail.com>
2025-04-12 04:24:23 +0200

    add commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Ecommerce-Angular-19
+
+#- Test Project by use NGRX And fake Api Data
+
+## Packages:
+#1- tailwindcss 
+#2- daisyui
+
+# NGRX => State Management:
+#3- ngrx/effects
+#4- ngrx/store
+#5- ngrx/store-devtools
+#6- ngrx-store-localstorage
+___________________________
+
+## BackEnd:
+#7 fakestoreapi.com
+
+## Pages:
+#8- Products Page
+#9- Product Details Page
+#10- Cart Page
+
+![Alt text](image-1.png)
+![Alt text](image-2.png)
+![Alt text](image-3.png)
+![Alt text](image-4.png)
--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -33,7 +33,7 @@ import { CartService } from '../../../cart/service/cart.service';
         </div>
         <div class="flex items-center justify-between">
             <span class="text-3xl font-bold text-orange-950">{{product.price | currency }}</span>
-            <a (click)="addToCart(product)"  class="text-white bg-orange-600 hover:bg-orange-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+            <a (click)="addToCart(product)" class="cursor-pointer text-white bg-orange-600 hover:bg-orange-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
             Add to cart
             </a>
         </div>
